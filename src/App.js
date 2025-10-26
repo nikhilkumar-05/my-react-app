af792663f9f0b20ce20242a486ada71c3fc96b65
Nikhil Kumar <nikhil.kumar225577@gmail.com>
2025-10-27 02:05:41 +0530

    App dark mode

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm' ;
+// import TextForm from './components/TextForm';  // Removed unused import
 
 function App() {
   return (
     <>
       <Navbar title="Text" aboutText="About Textutils" />
-      <div className="container my-3r ">
-      <TextForm heading ="Enter text here to analyze"/>
+      <div className="container my-3">
+        {/* <TextForm heading="Enter text here to analyze" /> */}  {/* Uncomment if needed */}
+        <About />
       </div>
     </>
   );
 }
 
-
 export default App;
- 
-
-
-
-
-
-
-
-
-
-
-
